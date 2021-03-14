18956d1d1032f2be0cb4ea08c66dbcbe57233cc6
Atakan Savas <a.savas@hotmail.com.tr>
2021-03-14 13:37:52 +0300

    moment js config updated

--- config/index.js
+++ config/index.js
@@ -2,6 +2,9 @@ const ApiUrls = require('./apiUrls');
 const DbConfigs = require('./dbConfigs');
 const TableNames = require('./tableNames');
 const AwsConfig = require('./awsConfig');
+const moment = require('moment');
+
+moment.tz.setDefault('Europa/Istanbul');
 
 module.exports = {
   ApiUrls,
