be666cae835278963ef7050eceb0c3dd82e5ce80
Atakan Savas <a.savas@hotmail.com.tr>
2021-03-21 17:08:29 +0300

    system settings updated

--- enums/settings/systemSettings.js
+++ enums/settings/systemSettings.js
@@ -3,11 +3,17 @@ const Enum = require('../../base/enum/index');
 const SystemSettings = new Enum({
   TCKNCheck: '1',
   KDVRate: '2',
-  PlannedOrderVisibleTime: '8',
   StepPrice: '4',
   AppVersion: '5',
   BridgePrice: '6',
+  PlannedOrderVisibleTime: '8',
+
   KMPrice: '7',
+
+  ToleranceDuration: '9',
+  TolerancePrice: '10',
+  PenaltyForCourierAssign: '11',
+  PenaltyForCourierPicked: '12',
 });
 
 module.exports = SystemSettings;
