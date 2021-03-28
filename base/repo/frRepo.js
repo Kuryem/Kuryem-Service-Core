5b4a3e4ec1c18a5cd8209d43c510699d0bbb9e82
atakansavas <a.savas@hotmail.com.tr>
2021-03-28 04:55:26 +0300

    id silindi

--- base/repo/frRepo.js
+++ base/repo/frRepo.js
@@ -25,6 +25,9 @@ const FrRepo = {
 
   //* Updates given document
   update: async (db, collection, document) => {
+    if (typeof document._id !== 'object' && document._id !== null) {
+      document._id = ObjectId(document._id);
+    }
     const _where = { _id: document._id };
 
     delete document._id;
